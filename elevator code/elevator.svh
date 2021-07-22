b0ebc192d413de14b8db466316ef8ebd5c507f23
kse8974 <43025968+kse8974@users.noreply.github.com>
2021-07-22 22:30:29 +0900

    Delete elevator code directory

--- elevator code/elevator.svh
+++
@@ -1,2 +0,0 @@
-      	
-parameter first = 8'b0000_0001, second = 8'b0000_0010,third = 8'b0000_0100, fourth = 8'b0000_1000, fifth = 8'b0001_0000,sixth=8'b0010_0000,seventh=8'b0100_0000,eighth=8'b1000_0000;	
